9746ac7a5965f309c12a4237130bce8a400758bd
Emeka <ilozuechukwuemeka@yahoo.com>
2022-10-19 15:57:40 +0100

    etl build

---
+++ C_HIV_Enrollment/HIV_Enrollment/HIV_Enrollment_run.ps1
@@ -0,0 +1,3 @@
+$fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+cd $fileDir
+java '-Dtalend.component.manager.m2.repository=%cd%/../lib' '-Xms256M' '-Xmx1024M' -cp '.;../lib/routines.jar;../lib/log4j-slf4j-impl-2.12.1.jar;../lib/log4j-api-2.12.1.jar;../lib/log4j-core-2.12.1.jar;../lib/log4j-1.2-api-2.12.1.jar;../lib/commons-collections-3.2.2.jar;../lib/jboss-serialization.jar;../lib/postgresql-42.2.9.jar;../lib/advancedPersistentLookupLib-1.2.jar;../lib/slf4j-api-1.7.25.jar;../lib/dom4j-2.1.1.jar;../lib/external_sort.jar;../lib/trove.jar;../lib/crypto-utils.jar;hiv_enrollment_6_0.jar;' lamisplus_etl.hiv_enrollment_6_0.HIV_Enrollment  --context=Lamisplus $args
